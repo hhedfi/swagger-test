374f1107c304d284f708ed56f860711e29fc5c0b
Hamza HADFI <hamza.hadfi@drbanner.com>
2019-10-01 22:54:01 +0200

    init project for swagger

---
+++ src/main/java/com/lina/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.lina.demo.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Api
+@RestController("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
